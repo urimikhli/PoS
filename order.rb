21fb33e88526ad6736d47458151fde8edf4e3328
Uri Mikhli <urimikhli@gmail.com>
2015-07-26 17:32:11 -0400

    Add items to order

--- order.rb
+++ order.rb
@@ -1,13 +1,23 @@
-require_relative 'price_list.rb' # PoS Terminal
+require_relative 'price_list.rb'
 
 class Order
+
   def initialize(price_list)
+    @order = []
     @price_list ||= price_list
   end
+
   def add_item(item_code)
     #
-    price = @price_list.price(item_code)
+    item = @price_list.get_item(item_code)
+    unless item.empty?
+      @order << { product_code: item_code, price: item["price"]}
+      item["price"]
+    else
+      "NO ITEM FOUND"
+    end
   end
+
   def total
     #
   end
