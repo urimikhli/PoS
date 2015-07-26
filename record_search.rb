2a09310dd0531b69ec2e571a87b77430ad877fb4
Uri Mikhli <urimikhli@gmail.com>
2015-07-26 16:20:50 -0400

    pass an array to record search

--- record_search.rb
+++ record_search.rb
@@ -4,12 +4,13 @@ module RecordSearch
     VALIDSEARCHFIELDS.select { |x| x.downcase == search_field.downcase }.empty? ? false : true
   end
 
-  def records_search(search_field, query_field)
-    @pricing.select { |x| /#{query_field.downcase}/.match(x[search_field].downcase) }.sort_by { |hsh| hsh[search_field] }
+  def search(search_space,search_field, query_field)
+    return unless validate_field search_field
+    records_search(search_space, search_field, query_field)
   end
 
-  def search_inventory(search_field, query_field)
-    return unless validate_field search_field
-    records_search(search_field, query_field)
+  private
+  def records_search(search_space, search_field, query_field)
+    search_space.select { |x| /#{query_field.downcase}/.match(x[search_field].downcase) }.sort_by { |hsh| hsh[search_field] }
   end
 end
