9fb0ae9308e1633d72f6d61f3703991a2695b107
Uri Mikhli <urimikhli@gmail.com>
2015-07-26 15:48:32 -0400

    First cut at top level object

--- terminal.rb
+++ terminal.rb
@@ -1,1 +1,30 @@
-# Terminal module
+#require_relative 'price_list.rb' # PoS Terminal
+require_relative 'order.rb' # PoS Terminal
+class Terminal
+  attr_accessor :price_list
+
+  def initialize
+    set_pricing # initialize as default, call set pricing to reassign: @order and @price_list = PriceList.new(some_file_name)
+  end
+
+  #new list and new order
+  def set_pricing(price_list = PriceList.new)
+    puts price_list.class
+    @price_list = price_list
+    @order = Order.new(price_list)
+    #price_list
+  end
+
+  def scan(item_code = "")
+    'scan: ' + item_code.to_s
+
+    price = @order.add_item(item_code)
+
+    puts "scan:#{item_code}:price: #{price}" unless price.empty?
+  end
+
+  def total
+    @order.total
+  end
+
+end
