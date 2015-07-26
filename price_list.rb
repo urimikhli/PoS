ce0b8a1859ac67ddd4f0b09dd1100c8a6628002f
Uri Mikhli <urimikhli@gmail.com>
2015-07-26 17:35:39 -0400

    load product list from file

--- price_list.rb
+++ price_list.rb
@@ -3,14 +3,12 @@ class PriceList
   include RecordSearch
 
   def initialize(file_name='./data/price_list.json') #set default, but give chane to give diff file
-    @pricing = set_price_list(filename)
+    @pricing = set_price_list(file_name)
   end
 
-  def set_price_list(filename)
-    'The price list'
-    #read file
-    # jason array
-    []
+  def set_price_list(file_name)
+    load_from_file(file_name)
+  end
   end
 
   def price(item_code)
@@ -30,6 +28,7 @@ class PriceList
   def get_price(item_code, quantity)
     if item = search_inventory(item_code, "product_code")
     end
+  def load_from_file(file_name)
+    JSON.parse(IO.read(file_name))
   end
-
 end
