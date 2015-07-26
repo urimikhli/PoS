3b8a4d5590b52bfdc44732c8236d483535483222
Uri Mikhli <urimikhli@gmail.com>
2015-07-26 17:36:17 -0400

    Focus on returning products

--- price_list.rb
+++ price_list.rb
@@ -1,18 +1,22 @@
+require 'json'
 require_relative 'record_search.rb'
+
 class PriceList
   include RecordSearch
 
-  def initialize(file_name='./data/price_list.json') #set default, but give chane to give diff file
+  def initialize(file_name = './data/price_list.json') #set default, but give chane to give diff file
     @pricing = set_price_list(file_name)
   end
 
   def set_price_list(file_name)
     load_from_file(file_name)
   end
+
+  def get_item(item_code)
+    search(@pricing, item_code, "product_code")
   end
 
   def price(item_code)
-    get_price(item_code, quantity)
   end
 
   def discount_point
@@ -25,9 +29,6 @@ class PriceList
 
   private
 
-  def get_price(item_code, quantity)
-    if item = search_inventory(item_code, "product_code")
-    end
   def load_from_file(file_name)
     JSON.parse(IO.read(file_name))
   end
