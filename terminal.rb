eef635be263005bc94361468fde85cb89b5a6342
Uri Mikhli <urimikhli@gmail.com>
2015-07-26 23:43:47 -0400

    Abstract the pricing file away from terminal

--- terminal.rb
+++ terminal.rb
@@ -6,10 +6,17 @@ class Terminal
     set_pricing # initialize as default, call set pricing to reassign: @order and @price_list = PriceList.new(some_file_name)
   end
 
+  def pricing_file(pricing_type="regular")
+    {
+        regular: './data/price_list.json',
+        holiday: './data/price_list.json'
+    }[pricing_type.to_sym]
+  end
+
   #new list and new order
-  def set_pricing(price_list = PriceList.new)
-    @price_list = price_list
-    @order = Order.new(price_list)
+  def set_pricing(pricing_type="regular")
+    @price_list = PriceList.new(pricing_file(pricing_type).to_s)
+    @order = Order.new(@price_list)
     #price_list
   end
 
@@ -19,6 +26,7 @@ class Terminal
     puts 'scan: ' + item_code.to_s + ":price:" + @price_list.price(item_code).to_s
 
   end
+
   def total
     @order.total
   end
