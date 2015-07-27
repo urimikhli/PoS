d595bbb07b43312102831605c3bc08d81b2c736d
Uri Mikhli <urimikhli@gmail.com>
2015-07-26 23:06:35 -0400

    Clean up and better message on scan

--- terminal.rb
+++ terminal.rb
@@ -1,7 +1,6 @@
-#require_relative 'price_list.rb' # PoS Terminal
-require_relative 'order.rb' # PoS Terminal
+require_relative 'order.rb'
 class Terminal
-  attr_accessor :price_list
+  attr_accessor :price_list, :order
 
   def initialize
     set_pricing # initialize as default, call set pricing to reassign: @order and @price_list = PriceList.new(some_file_name)
@@ -9,19 +8,17 @@ class Terminal
 
   #new list and new order
   def set_pricing(price_list = PriceList.new)
-    puts price_list.class
     @price_list = price_list
     @order = Order.new(price_list)
     #price_list
   end
 
   def scan(item_code = "")
-    puts 'scan: ' + item_code.to_s
-
     @order.add_item(item_code)
 
-  end
+    puts 'scan: ' + item_code.to_s + ":price:" + @price_list.price(item_code).to_s
 
+  end
   def total
     @order.total
   end
