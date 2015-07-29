5d0b4c482ced6dd090ae3eeb126611683768aa32
Uri Mikhli <urimikhli@gmail.com>
2015-07-29 01:05:19 -0400

    Working spec for top level Terminal class

--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -4,6 +4,31 @@ describe Terminal do
 
   let(:terminal) { Terminal.new }
 
+  it 'should have pricing model' do
+    expect(terminal.price_list.is_a? PriceList).to be true
+  end
+  it 'should have an order' do
+    expect(terminal.order.is_a? Order).to be true
+  end
+
+  describe '.set_pricing' do
+
+    it { is_expected.to respond_to :set_pricing }
+    it 'should use regular pricing' do
+      expect(terminal.price_list.price("A")).to equal 2.00
+    end
+    context 'Changing the pricing model ' do
+      before do
+        terminal.set_pricing('holiday')
+      end
+
+      it 'should have a different price' do
+        expect(terminal.price_list.price("A")).to equal 3.00
+      end
+    end
+
+  end
+
   describe '.scan' do
     it { is_expected.to respond_to :scan }
     it 'should have no orders' do
@@ -36,7 +61,30 @@ describe Terminal do
     end
   end
 
-  describe ''
+  describe '.new_order' do
+
+    before do
+      terminal.scan("A")
+    end
+
+    it { is_expected.to respond_to :new_order }
+
+    it 'should have an order with regular total for product A' do
+      expect(terminal.order.order.count).to equal 1
+      expect(terminal.total).to equal 2.00
+    end
+
+    context 'Calling new_order creates a new order object' do
+      before do
+        terminal.new_order
+      end
+      it 'should show a different total ' do
+        expect(terminal.order.order.count).to equal 0
+        expect(terminal.total).to equal 0
+      end
+    end
+  end
+
 
 end
 
