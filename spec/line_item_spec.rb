8f06f03a68d31e82a32b02db2447f3e8423cc7b0
Uri Mikhli <urimikhli@gmail.com>
2015-08-06 14:40:15 -0400

    spec for the local volume pricing accessor methods

--- spec/line_item_spec.rb
+++ spec/line_item_spec.rb
@@ -2,6 +2,15 @@ require 'rspec'
 require './line_item'
 
 describe 'LineItem' do
+  let (:no_discount_item_hash) {
+    {
+        'product_code'=> 'B',
+        'price'=> 12.00,
+        'discounts'=> []
+    }
+
+  }
+
   let(:item_hash) {
     {
         'product_code'=> 'A',
@@ -14,6 +23,7 @@ describe 'LineItem' do
         ]
     }
   }
+  let(:line_item_no_discount) {LineItem.new(no_discount_item_hash)}
   let(:line_item) {LineItem.new(item_hash)}
   subject {line_item}
 
@@ -78,7 +88,6 @@ describe 'LineItem' do
     end
   end
 
-
   describe '.price' do
     it { is_expected.to respond_to :price }
 
@@ -86,5 +95,39 @@ describe 'LineItem' do
       expect(line_item.price).to equal 2.00
     end
   end
+
+  describe '.product_code' do
+    it { is_expected.to respond_to :product_code }
+
+    it 'should return the correct line item price' do
+      expect(line_item.product_code).to eq 'A'
+    end
+  end
+
+  describe '.discount_total' do
+    it { is_expected.to respond_to :discount_total }
+
+    it 'should return the discount total' do
+      expect(line_item.discount_total).to eq 7.00
+    end
+
+     it 'if there is no discount pricing then it should return nil for discount_total' do
+      expect(line_item_no_discount.discount_total).to be_nil
+     end
+  end
+
+  describe '.discount_point' do
+    it { is_expected.to respond_to :discount_point }
+
+    it 'should return the discount point' do
+      expect(line_item.discount_point).to eq 4
+    end
+
+    it 'if there is no discount pricing then it should return nil for discount_point' do
+      expect(line_item_no_discount.discount_point).to be_nil
+    end
+  end
+
+
 
 end
